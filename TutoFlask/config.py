9b1419ef40123df98c1473db6de76b5d0e3ce76e
korasrar <celestin.maubert@etu.univ-orleans.fr>
2025-09-03 15:15:45 +0200

    tp1 dev_web : ajout des pages about/ et contact/ ( hyperlink to github )

--- TutoFlask/config.py
+++ TutoFlask/config.py
@@ -1,3 +1,5 @@
 #>>>import random, string, os
 #>>>"".join([random.choice(string.printable) for _ in os.urandom(24) ] )
 SECRET_KEY = "k_r&Kw?]ERsmw1@lx-Bow}4|"
+ABOUT = "Bienvenue sur la page à propos de Flask !"
+CONTACT = "<a href=""https://github.com/korasrar"">Github : @korasrar</a> "
--- TutoFlask/monApp/views.py
+++ TutoFlask/monApp/views.py
@@ -1,8 +1,18 @@
 from .app import app
+from config import ABOUT
+from config import CONTACT
 
 @app.route('/')
 def index():
     return "Hello world !"
+
+@app.route('/about/')
+def about():
+    return ABOUT
+
+@app.route('/contact/')
+def contact():
+    return CONTACT
 
 if __name__ == "__main__":
     app.run()
