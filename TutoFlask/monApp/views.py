fec0125832dc947bc310b20554c839b5c57d177e
korasrar <celestin.maubert@etu.univ-orleans.fr>
2025-09-17 14:11:00 +0200

    tp4 dev_web : ajout de base.html (héritage) + style a l'aide de bootstrap

--- TutoFlask/monApp/views.py
+++ TutoFlask/monApp/views.py
@@ -1,12 +1,19 @@
 from .app import app
-from flask import render_template
+from flask import render_template, request
 from config import ABOUT
 from config import CONTACT
+from monApp.models import Auteur
+from monApp.models import Livre
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template("index.html",title ="R3.01 Dev Web avec Flask",name="Cricri")
+    # si pas de paramètres
+    if len(request.args)==0:
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name="Cricri")
+    else :
+        param_name = request.args.get('name')
+        return render_template("index.html",title="R3.01 Dev Web avec Flask",name=param_name)
 
 @app.route('/about/')
 def about():
@@ -15,6 +22,17 @@ def about():
 @app.route('/contact/')
 def contact():
     return render_template("contact.html",title ="Dev Web | Contact")
+
+
+@app.route('/auteurs/')
+def getAuteurs():
+    lesAuteurs = Auteur.query.all()
+    return render_template('auteurs_list.html', title="R3.01 Dev Web avec Flask", auteurs=lesAuteurs)
+
+@app.route('/livres/')
+def getLivres():
+    lesLivres = Livre.query.all()
+    return render_template('livres_list.html', title="R3.01 Dev Web avec Flask", livres=lesLivres)
 
 if __name__ == "__main__":
     app.run()
